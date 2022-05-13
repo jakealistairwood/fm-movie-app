cb127cdebd298b7dbf18a999f11e540e88835f2d
Jake Wood <jakealistairwood@gmail.com>
2022-05-13 15:41:26 +0100

    added responsiveness to grid layout

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -2,35 +2,42 @@ import React from "react";
 import "./MovieCard.scss";
 import TVSeriesIcon from "../../assets/icon-category-tv.svg";
 import MovieIcon from "../../assets/icon-category-movie.svg";
-import BookmarkIcon from "../../assets/icon-bookmark-empty.svg";
+import BookmarkEmpty from "../../assets/icon-bookmark-empty.svg";
+import BookmarkFull from "../../assets/icon-bookmark-full.svg";
 
 const MovieCard = ({ movie }) => {
+    const { title, rating, year, category, thumbnail, isBookmarked } = movie;
+
     const renderCategoryIcon =
-        movie.category == "TV Series" ? TVSeriesIcon : MovieIcon;
+        category == "TV Series" ? TVSeriesIcon : MovieIcon;
 
     return (
         <div className="movie-card">
             <div
                 className="movie-card__img"
                 style={{
-                    backgroundImage: `url(${movie.thumbnail.regular.large})`,
+                    backgroundImage: `url(${thumbnail.regular.large})`,
                 }}
             >
                 <div className="trending-movie__bookmark">
-                    <img src={BookmarkIcon} alt="add-to-favorites" />
+                    {isBookmarked ? (
+                        <img src={BookmarkFull} alt="add-to-favorites" />
+                    ) : (
+                        <img src={BookmarkEmpty} alt="add-to-favorites" />
+                    )}
                 </div>
             </div>
             <div className="movie-card__info">
-                <p className="sm-text">{movie.year}</p>
+                <p className="sm-text">{year}</p>
                 <div className="circle-detail"></div>
                 <div className="movie-card__category">
-                    <img src={renderCategoryIcon} alt={movie.category} />
-                    <p className="sm-text">{movie.category}</p>
+                    <img src={renderCategoryIcon} alt={category} />
+                    <p className="sm-text">{category}</p>
                 </div>
                 <div className="circle-detail"></div>
-                <p className="sm-text">{movie.rating}</p>
+                <p className="sm-text">{rating}</p>
             </div>
-            <h5 className="movie-card__title">{movie.title}</h5>
+            <h5 className="movie-card__title">{title}</h5>
         </div>
     );
 };
